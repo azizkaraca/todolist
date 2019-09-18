4a5ce733b836cace33ec1b79bbf7e00c30206b63
Aziz KARACA <54979031+azizkaraca@users.noreply.github.com>
2019-09-18 15:48:47 +0300

    Add files via upload

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { callbackify } from 'util';
+import { Key } from 'protractor';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  data = {
+    pendings: [
+      'Get to work',
+      'Pick up groceries',
+      'Go home',
+      'Fall asleep'
+    ],
+    inProgress: [
+      'Have breakfast',
+      'Wear clothes'
+    ],
+    done: [
+      'Get up',
+      'Brush teeth',
+      'Take a shower',
+      'Check e-mail',
+      'Walk dog'
+    ]    
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+    this.setItems();
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+        Object.keys(this.data).forEach((key)=>{
+          localStorage.setItem(key, JSON.stringify(this.data[key]));
+        });
+    }
+  }
+
+  addTodo(todo) {
+    this.data.pendings.push(todo.value);
+    todo.value = "";
+    localStorage.setItem('pendings', JSON.stringify(this.data.pendings));
+  }
+
+  setItems() {
+    Object.keys(this.data).forEach((key)=>{
+      if (!localStorage.getItem(key)) {
+        localStorage.setItem(key, JSON.stringify(this.data[key]));
+      } else {
+        this.data[key] = JSON.parse(localStorage.getItem(key));
+      }
+    });
+  }
+}
---
+++ todolist/README.md
@@ -0,0 +1,2 @@
+# todolist
+ This app keeps your to-do list. However, you can add new tasks to this list and update your current stage.
